12bf00e20b96bc0fced6ffd59011376c59a755c2
FritzJo <fritzjo-git@mailbox.org>
2021-05-09 13:00:30 +0200

    back to initial screenshot code to avoid bugs..

--- client/src/myazo.py
+++ client/src/myazo.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python3
-from screenshot import Myazo
+import wx.adv
+import wx
 
-if __name__ == "__main__":
-    m = Myazo()
-    m.take_screenshot()
+from screenshot import take_screenshot
+
+TRAY_TOOLTIP = 'Name'
+TRAY_ICON = 'tray.png'
+
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.Append(item)
+    return item
+
+
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    def __init__(self, frame):
+        self.frame = frame
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        # menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def set_icon(self, path):
+        icon = wx.Icon(path)
+        self.SetIcon(icon, TRAY_TOOLTIP)
+
+    def on_left_down(self, event):
+        print('Tray icon was left-clicked.')
+        # self.m.take_screenshot()
+        take_screenshot()
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+        self.frame.Close()
+
+
+class App(wx.App):
+    def OnInit(self):
+        frame = wx.Frame(None)
+        self.SetTopWindow(frame)
+        TaskBarIcon(frame)
+        return True
+
+
+def main():
+    app = App(False)
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
--- client/src/screenshot.py
+++ client/src/screenshot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import os
 import platform
 import shutil
@@ -13,107 +14,98 @@ import requests
 from PIL import Image
 
 
-class Configuration():
-    def __init__(self):
-        config_parser = ConfigParser()
-        config_parser.read_dict(
-            {
-                "Myazo": {
-                    "gyazo_server": False,  # If True, upload_script and secret are ignored
-                    "gyazo_direct_link": True,  # Ignored if gyazo_server is False
-                    "upload_script": "https://myazo.example.com/upload.php",
-                    "secret": "hunter2",
-                    "clear_metadata": True,
-                    "open_browser": True,
-                    "copy_clipboard": True,
-                    "output_url": True,
-                }
+def take_screenshot():
+    # Configuration
+    config_parser = ConfigParser()
+    config_parser.read_dict(
+        {
+            "Myazo": {
+                "gyazo_server": False,  # If True, upload_script and secret are ignored
+                "gyazo_direct_link": True,  # Ignored if gyazo_server is False
+                "upload_script": "https://myazo.example.com/upload.php",
+                "secret": "hunter2",
+                "clear_metadata": True,
+                "open_browser": True,
+                "copy_clipboard": True,
+                "output_url": True,
             }
-        )
-        config_parser.read(os.path.expanduser("~/.config/myazo/config.ini"))
-        self.config = config_parser["Myazo"]
-
-    def get_backend(self, system, tmp_filename):
-        backends = {
-            "Linux": {
-                "gnome-screenshot": ["-a", "-f", tmp_filename],
-                "mv": ["$(xfce4-screenshooter -r -o ls)", tmp_filename],
-                # KDE Spectacle requires slight user interaction after selecting region
-                "spectacle": ["-b", "-n", "-r", "-o", tmp_filename],
-                "scrot": ["-s", tmp_filename],
-                # ImageMagick
-                "import": [tmp_filename],
-            },
-            # macOS
-            "Darwin": {"screencapture": ["-i", tmp_filename]},
-            # '/clip' requires at least Win10 1703
-            "Windows": {"snippingtool": ["/clip"]},
         }
-        util = None
-        for util, args in backends[system].items():
-            if shutil.which(util) is not None and run([util] + args).returncode == 0:
-                break
-        return util
+    )
+    config_parser.read(os.path.expanduser("~/.config/myazo/config.ini"))
+    config = config_parser["Myazo"]
 
+    tmp_filename = tempfile.NamedTemporaryFile(suffix=".png").name
 
-class Myazo:
-    def __init__(self):
-        c = Configuration()
-        self.tmp_filename = tempfile.NamedTemporaryFile(suffix=".png").name
-        self.conf = c.config
-        self.backend = c.get_backend(platform.system(), self.tmp_filename)
+    backends = {
+        "Linux": {
+            "gnome-screenshot": ["-a", "-f", tmp_filename],
+            "mv": ["$(xfce4-screenshooter -r -o ls)", tmp_filename],
+            # KDE Spectacle requires slight user interaction after selecting region
+            "spectacle": ["-b", "-n", "-r", "-o", tmp_filename],
+            "scrot": ["-s", tmp_filename],
+            # ImageMagick
+            "import": [tmp_filename],
+        },
+        # macOS
+        "Darwin": {"screencapture": ["-i", tmp_filename]},
+        # '/clip' requires at least Win10 1703
+        "Windows": {"snippingtool": ["/clip"]},
+    }
 
-    def take_screenshot(self):
+    util = None
+    for util, args in backends[platform.system()].items():
+        if shutil.which(util) is not None and run([util] + args).returncode == 0:
+            break
 
-        if self.backend == "snippingtool":
-            from PIL import ImageGrab
+    # If the used util stored the screenshot in the clipboard, output it to the tmp file
+    if util == "snippingtool":
+        from PIL import ImageGrab
 
-            img = ImageGrab.grabclipboard()
-            if img is not None:
-                img.save(self.tmp_filename, optimize=True)
+        img = ImageGrab.grabclipboard()
+        if img is not None:
+            img.save(tmp_filename, optimize=True)
 
-        if os.path.isfile(self.tmp_filename) is not True:
-            print("Error: Failed to take screenshot.")
-            exit(1)
+    if os.path.isfile(tmp_filename) is not True:
+        print("Error: Failed to take screenshot.")
+        exit(1)
 
-        if self.conf.getboolean("clear_metadata"):
-            img = Image.open(self.tmp_filename)
-            new_img = Image.new(img.mode, img.size)
-            new_img.putdata(list(img.getdata()))
-            new_img.save(self.tmp_filename, optimize=True)
+    if config.getboolean("clear_metadata"):
+        img = Image.open(tmp_filename)
+        new_img = Image.new(img.mode, img.size)
+        new_img.putdata(list(img.getdata()))
+        new_img.save(tmp_filename, optimize=True)
 
-        # Upload image to server
-        img = open(self.tmp_filename, "rb")
+    # Upload image to server
+    img = open(tmp_filename, "rb")
+    if config.getboolean("gyazo_server"):
+        r = requests.post("https://upload.gyazo.com/upload.cgi", files={"imagedata": img})
+    else:
+        r = requests.post(
+            config.get("upload_script"),
+            data={"secret": config.get("secret")},
+            files={"screenshot": img},
+        )
 
-        if self.conf.getboolean("gyazo_server"):
-            r = requests.post("https://upload.gyazo.com/upload.cgi", files={"imagedata": img})
-        else:
-            r = requests.post(
-                self.conf.get("upload_script"),
-                data={"secret": self.conf.get("secret")},
-                files={"screenshot": img},
-            )
+    if r.status_code != 200:
+        print(
+            "Error: Failed to upload screenshot. "
+            f"Server returned status code '{r.status_code}'."
+        )
+        exit(2)
 
-        if r.status_code != 200:
-            print(
-                "Error: Failed to upload screenshot. "
-                f"Server returned status code '{r.status_code}'."
-            )
-            exit(2)
+    if config.getboolean("gyazo_server") and config.getboolean("gyazo_direct_link"):
+        # Convert the Gyazo link to a direct image
+        # https://gyazo.com/hash > https://i.gyazo.com/hash.extension
+        url = r.text.replace("//", "//i.") + Path(tmp_filename).suffix
+    else:
+        url = r.text
 
-        if self.conf.getboolean("gyazo_server") and self.conf.getboolean("gyazo_direct_link"):
-            # Convert the Gyazo link to a direct image
-            # https://gyazo.com/hash > https://i.gyazo.com/hash.extension
-            url = r.text.replace("//", "//i.") + Path(self.tmp_filename).suffix
-        else:
-            url = r.text
+    if config.getboolean("open_browser"):
+        webbrowser.open(url)
+    if config.getboolean("copy_clipboard"):
+        pyperclip.copy(url)
+    if config.getboolean("output_url"):
+        print(url)
 
-        if self.conf.getboolean("open_browser"):
-            webbrowser.open(url)
-        if self.conf.getboolean("copy_clipboard"):
-            pyperclip.copy(url)
-        if self.conf.getboolean("output_url"):
-            print(url)
-
-        img.close()
-        os.remove(self.tmp_filename)
+    img.close()
+    os.remove(tmp_filename)
